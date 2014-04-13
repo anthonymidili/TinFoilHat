858304f50e2d958067f0931d987e4d5315529d23
Anthony M. Midili <tonywinslow@yahoo.com>
2014-04-13 11:19:43 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ManOnFire::Application.config.secret_key_base = 'b1902039bce113fa4e0bf4bcc433f6256b532204bc68c91b323373607f36a8ad25adbe119abf6282c83918e4c065e00047ae6a09bddad4fc5993465798dcee66'
