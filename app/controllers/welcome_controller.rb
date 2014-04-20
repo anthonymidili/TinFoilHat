8c030d672f2386bb37bdf19a7f5a020a72f78e44
Anthony M. Midili <tonywinslow@yahoo.com>
2014-04-20 12:51:10 -0400

    Mock views. Add Frankie's github public activity feed.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,11 @@
 class WelcomeController < ApplicationController
   def home
-    fp = FeedParser.new(:url => 'https://github.com/anthonymidili.atom',
+    anthony_midili = FeedParser.new(:url => 'https://github.com/anthonymidili.atom',
                         :http => {:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE})
-    @feed = fp.parse
+    @am_feed = anthony_midili.parse
+
+    frank_mattia = FeedParser.new(:url => 'https://github.com/frankjmattia.atom',
+                        :http => {:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE})
+    @fm_feed = frank_mattia.parse
   end
 end
