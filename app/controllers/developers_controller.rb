bb37e942ed4ccf62b9692c2ff7fd43f820ef6e80
AMidili <tonywinslow@yahoo.com>
2017-12-03 22:25:46 -0500

    Refactor, refactor, refactor. Add a developers bio pages with a short description to get started. Start adding content to the about page.

---
+++ app/controllers/developers_controller.rb
@@ -0,0 +1,9 @@
+class DevelopersController < ApplicationController
+  def frank_mattia
+    render 'developers/frank_mattia/bio'
+  end
+
+  def anthony_midili
+    render 'developers/anthony_midili/bio'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'welcome#home'
+
   get :about, to: 'welcome#about'
+  scope :about do
+    get :anthony_midili, to: 'developers#anthony_midili'
+    get :frank_mattia, to: 'developers#frank_mattia'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
